a9ca1ca24d2a9f040b8f1fd9bc761e137ad258f5
Josh Nahum <nahumjos@msu.edu>
2017-04-17 12:56:24 -0400

    using hackerrank on HW #9

--- hackerrank_api.py
+++ hackerrank_api.py
@@ -1,6 +1,15 @@
 import requests
 import os.path
 from pprint import pprint
+import os
+import string
+
+
+def sanitize(input_):
+    allowed_chars = string.ascii_letters + string.digits
+    letters = [letter if letter in allowed_chars else '_' for letter in input_]
+    return "".join(letters)
+
 
 output_filename = "output.html"
 
@@ -14,8 +23,8 @@ r = requests.get(base_url + test_url, params=payload)
 response = r.json()
 tests = response['data']
 id_names = {test["name"]: test["id"] for test in tests}
-print(id_names)
-test_name = "CSE480 Homework #6"
+# print(id_names)
+test_name = "CSE480 Homework #9"
 test_id = id_names[test_name]
 
 test_results_url = "/tests/" + str(test_id) + "/candidates/"
@@ -23,25 +32,21 @@ payload = {'access_token': token}
 r = requests.get(base_url + test_results_url, params=payload)
 response = r.json()
 completed = response['data']
-pprint(completed)
+# pprint(completed)
+
+test_name = sanitize(test_name)
+os.makedirs(test_name, exist_ok=True)
 
 email_to_response = {}
 for student in completed:
     email = student["email"]
+    student_folder = os.path.join(test_name, email)
+    os.makedirs(student_folder, exist_ok=True)
     if "questions" not in student:
-        response = "<p>No submission</p>"
-    else:
-        answers = [q["answer"] for q in student["questions"]]
-        if len(answers) < 7:
-            response = "<p>No Limerick</p>"
-        else:
-            response = answers[6]
-    email_to_response[email] = response
-
-output = ["<h1>limericks</h1>"]
-for email, response in email_to_response.items():
-    output.append("<h2>{}</h2>".format(email))
-    output.append(response)
-
-with open(output_filename, 'w') as output_fp:
-    output_fp.writelines(output)
+        break
+    for question_number, question in enumerate(student["questions"]):
+        solution = question["answer"]
+        solution_path = os.path.join(student_folder,
+                                     "q" + str(question_number + 1) + ".txt")
+        with open(solution_path, "w") as file_handle:
+            file_handle.writelines(solution)
